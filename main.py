d6aadfa3c94e63b3bce7dde1c5202e3eed9f594d
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-06-13 16:01:00 +0800

    edit stream message

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ from fastapi.responses import StreamingResponse
 app = FastAPI()
 
 message = """
-    Rick: (stumbles in drunkenly, and turns on the lights) Morty! You gotta come on. You got--... you gotta come with me.
-    Morty: (rubs his eyes) What, Rick? What's going on?
-    Rick: I got a surprise for you, Morty.
-    Morty: It's the middle of the night. What are you talking about?
-    Rick: (spills alcohol on Morty's bed) Come on, I got a surprise for you. (drags Morty by the ankle) Come on, hurry up. (pulls Morty out of his bed and into the hall)
-    Morty: Ow! Ow! You're tugging me too hard!
-    Rick: We gotta go, gotta get outta here, come on. Got a surprise for you Morty.
+            Streaming from VPS
+            This is doekdiem
+            This is developer
 """
 
 @app.get("/stream", response_class=StreamingResponse)
